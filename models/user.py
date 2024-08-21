ae8a7ce1ad6f264ce1498972d7dedd54a0caacdd
Oyurchu <oyurchuk@modeln.com>
2024-08-21 15:16:05 +0300

    - Added db service in docker-compose

--- models/user.py
+++ models/user.py
@@ -4,4 +4,4 @@ from db import db
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
